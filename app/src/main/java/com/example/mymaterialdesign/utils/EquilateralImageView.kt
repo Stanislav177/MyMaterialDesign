bbfb4b914fcbf8969f6f31e5c744237d22560c04
Stanislav <stas.177@mail.ru>
2022-04-11 12:37:30 +0300

    Урок - 1 создан кастомный imageView

---
+++ app/src/main/java/com/example/mymaterialdesign/utils/EquilateralImageView.kt
@@ -0,0 +1,17 @@
+package com.example.mymaterialdesign.utils
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatImageView
+
+class EquilateralImageView(
+    context: Context,
+    attributeSet: AttributeSet? = null,
+    defStyle: Int = 0
+) : AppCompatImageView(context, attributeSet, defStyle) {
+
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, widthMeasureSpec)
+    }
+}
